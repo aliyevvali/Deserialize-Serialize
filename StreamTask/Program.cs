477118737c5d9bd4edcc1985d0ce6f4050e3e9c1
Ali Aliyev <tu4pw6wq5@aztu.local>
2022-04-06 14:01:15 +0400

    added task

--- StreamTask/Program.cs
+++ StreamTask/Program.cs
@@ -16,10 +16,17 @@ namespace StreamTask
             //Product bread = new Product { Id = 2, Name = "Nur", Price = 0.5 };
             //Product beer = new Product { Id = 3, Name = "Nzs", Price = 1.5 };
             //Product milk = new Product { Id = 3, Name = "Milla", Price = 1.5 };
-            //OrderItem item1 = new OrderItem { Id = 1, Product = smoke, Count = 1, TotalPrice = smoke.Price * 1 };
+            //OrderItem item1 = new OrderItem { Id = 1, Product = smoke, Count = 1 };
+            //item1.TotalPrice = smoke.Price * item1.Count;
+
             //OrderItem item2 = new OrderItem { Id = 2, Product = bread, Count = 2, TotalPrice = bread.Price * 2 };
+            //item2.TotalPrice = bread.Price * item2.Count;
+
             //OrderItem item3 = new OrderItem { Id = 3, Product = beer, Count = 4, TotalPrice = beer.Price * 4 };
+            //item3.TotalPrice = beer.Price * item3.Count;
+
             //OrderItem item4 = new OrderItem { Id = 4, Product = milk, Count = 1, TotalPrice = milk.Price * 1 };
+            //item4.TotalPrice = milk.Price * item4.Count;
             //List<OrderItem> orderItems1 = new List<OrderItem>();
             //orderItems1.Add(item1);
             //orderItems1.Add(item2);
@@ -44,8 +51,9 @@ namespace StreamTask
 
             foreach (var item in o1.OrderItems)
             {
-                Console.WriteLine(item.Product.Name);
+                Console.WriteLine($"Id :{item.Id} Name : {item.Product.Name} TotalPrice : {item.TotalPrice} price : {item.Product.Price}");
             }
+            
             #endregion
         }
     }
